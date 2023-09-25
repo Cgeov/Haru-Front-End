92f2ffd57fe1e49be09a4ff1b1ab85fed1cf30a1
Cgeov <cristobar2016@gmail.com>
2023-09-24 20:39:37 -0600

    Arreglo de rutas e inicialización de Navbar

---
+++ src/pages/about-us/index.js
@@ -0,0 +1,5 @@
+import Layout from "../layout";
+
+export default function AboutUs(){
+    return <Layout><div className="text-white">Sobre Nosotros</div></Layout>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inter: ['Inter', 'sans'],
+        interBold: ['Inter-Bold', 'sans'],
+        berkshire: ['Berkshire Swash', 'cursive'],
       },
+      colors:{
+          primary: '#7c2750',
+          secondary: '#fa5992'
+      }
     },
   },
-  plugins: [],
+  plugins: [
+   
+  ]
 }
