f761978f922404928058fdd5950a8d0eed6c0ea2
Bmendez01 <bamh.mendez.2001@gmail.com>
2023-11-04 07:47:53 -0600

    integracion de Login

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -3,38 +3,53 @@ import Link from "next/link";
 import { useContext, useState } from "react";
 
 export default function Register() {
-  
-  const [name, setName] = useState('');
-  const [lastname, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState(null);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {login} = useContext(ContextUser);
+
+
+
+  let dataRegister = {
+    name: name,
+    lastname: lastName,
+    email:email,
+    password: password,
+  }
 
   let headers = new Headers();
-
   headers.append("Content-Type", "application/json");
   headers.append("Accept", "application/json");
   headers.append("Access-Control-Allow-Origin", "http://localhost:3000");
   headers.append("Access-Control-Allow-Credentials", "true");
   headers.append("GET", "POST", "OPTIONS");
-
-  const sendRequest = event =>{
-    setErrors(null);
-    
-
-
+  const handleSubmitSignUp = async (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/auth/sign-up", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(dataRegister),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        login(data);
+      })
+      .catch((error) => {
+      });
   }
 
   return (
-    <div className="w-[80%] flex flex-row gap-[15px] justify-between my-20 mx-auto max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-[80%] flex flex-row gap-[15px] justify-between my-20 mx-auto max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
       <form onSubmit={handleSubmitSignUp}  className="space-y-6 w-[100%] max-w-xs" >
-        <h5 className="text-xl font-medium text-primary dark:text-white">
+        <h5 className="text-xl font-medium text-primary">
           Registrate en nuestra Plataforma
         </h5>
         <div>
           <label
             htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            className="block mb-2 text-sm font-medium text-gray-900">
             Correo Electronico
           </label>
           <input
@@ -42,17 +57,15 @@ export default function Register() {
             name="email"
             id="email"
             onChange={(e)=> setEmail(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="correo@correo.com"
-            onChange={(event) => setEmail(event.target.value)}
             required
-            
           />
         </div>
         <div>
           <label
-            htmlFor="nombre"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900">
             Nombre
           </label>
           <input
@@ -60,16 +73,15 @@ export default function Register() {
             name="name"
             id="name"
             onChange={(e)=> setName(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Nombre"
-            onChange={(event) => setName(event.target.value)}
             required
           />
         </div>
         <div>
           <label
-            htmlFor="apellido"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900">
             Apellidos
           </label>
           <input
@@ -77,16 +89,15 @@ export default function Register() {
             name="apellidos"
             id="apellidos"
             onChange={(e)=> setLastName(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Apellido"
-            onChange={(event) => setLastName(event.target.value)}
             required
           />
         </div>
         <div>
           <label
             htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            className="block mb-2 text-sm font-medium text-gray-900">
             Contraseña
           </label>
           <input
@@ -95,21 +106,20 @@ export default function Register() {
             onChange={(e)=> setPassword(e.target.value)}
             id="password"
             placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            onChange={(event) => setPassword(event.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required
           />
         </div>
         <button
           type="submit"
-          className="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          className="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
           Registrar
         </button>
-        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+        <div className="text-sm font-medium text-gray-500">
           ¿Ya posees una cuenta?{" "}
           <Link
             href="/login"
-            className=" text-primary hover:underline dark:text-blue-500">
+            className=" text-primary hover:underline">
             Inicia Sesión
           </Link>
         </div>
