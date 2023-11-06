15a986931e39998f7377bd981ad14dc73e009bb0
Christian Tobar <ctobar@premte.com>
2023-11-06 07:11:30 -0600

    Corrección Responsive Sección Cotización

--- src/components/quote/quote.jsx
+++ src/components/quote/quote.jsx
@@ -1,6 +1,6 @@
 export default function Quote() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-white">
+    <div className="w-screen bg-white">
       <div className="container mx-auto my-4 px-4 lg:px-20">
         <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
           <div className="flex">
