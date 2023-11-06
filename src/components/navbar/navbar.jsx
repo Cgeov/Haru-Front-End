75ab5ca27c28c401c11c9f7ed53b24ef2ddebcdc
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2023-11-06 00:29:27 -0600

    Pagina Cotizacion

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -102,7 +102,7 @@ export default function Navbar() {
             Galería
           </Link>
           <Link
-            href={"/gallery"}
+            href={"/quote"}
             className="text-lg font-semibold leading-6 text-primary">
             Cotizaciones
           </Link>
