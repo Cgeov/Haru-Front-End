9866d0bb3ea29b9c677a2305d29e1dc9b3571618
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2023-11-06 19:02:27 -0600

    Actualizacion imagenes

--- src/components/quote/quote-services.jsx
+++ src/components/quote/quote-services.jsx
@@ -18,21 +18,21 @@ export default function QuoteServices() {
             <div className="md:p-2 p-1 w-1/2">
               <img
                 alt="gallery"
-                className="w-full object-cover h-full object-center block"
+                className="w-full object-cover h-full object-center block rounded-lg"
                 src="https://th.bing.com/th/id/OIP.DfbxcDkTjlhjC3Yxf35XgQAAAA?pid=ImgDet&rs=1"
               />
             </div>
             <div className="md:p-2 p-1 w-1/2">
               <img
                 alt="gallery"
-                className="w-full object-cover h-full object-center block"
+                className="w-full object-cover h-full object-center block rounded-lg"
                 src="https://floristeriaretiro.files.wordpress.com/2021/04/roses-757656_1920.jpg"
               />
             </div>
             <div className="md:p-2 p-1 w-full">
               <img
                 alt="gallery"
-                className="w-full h-full object-cover object-center block"
+                className="w-full h-full object-cover object-center block rounded-lg"
                 src="https://th.bing.com/th/id/R.15f89313f7a6804f95195c0d4ac301b0?rik=2GqTIVTxy7CzNQ&riu=http%3a%2f%2fhandlebarstudios.com%2fwp-content%2fuploads%2f2014%2f09%2f14-2464-post%2fhandebar_studios_Destination_Wedding_Photograper_Lake_Tahoe_Estate_Wedding_Oritz58(pp_w992_h661).jpg&ehk=stzQepmWW%2fNWcwvqnDQmO5xp%2b4ZkkaiXH0YBO9JhJyY%3d&risl=&pid=ImgRaw&r=0"
               />
             </div>
@@ -41,21 +41,21 @@ export default function QuoteServices() {
             <div className="md:p-2 p-1 w-full">
               <img
                 alt="gallery"
-                className="w-full h-full object-cover object-center block"
+                className="w-full h-full object-cover object-center block rounded-lg"
                 src="https://assets.website-files.com/5d5c5c8b99f8052b6b48d49e/5d60506795034342730d4617_Outdoor%20Ceremonies%20x1-p-1080.jpeg"
               />
             </div>
             <div className="md:p-2 p-1 w-1/2">
               <img
                 alt="gallery"
-                className="w-full object-cover h-full object-center block"
+                className="w-full object-cover h-full object-center block rounded-lg"
                 src="https://www.tanatorioelsalvador.com/images/flores.jpg"
               />
             </div>
             <div className="md:p-2 p-1 w-1/2">
               <img
                 alt="gallery"
-                className="w-full object-cover h-full object-center block"
+                className="w-full object-cover h-full object-center block rounded-lg"
                 src="https://i.etsystatic.com/27432774/c/1566/1566/0/0/il/4220cd/4785415946/il_600x600.4785415946_nlou.jpg"
               />
             </div>
