420b1947367fdc1b601f9781bedb521a4f783e8d
ToshiKaji <toshicaballero@outlook.com>
2023-11-15 01:17:10 -0600

    Filtro de tabla y actualizacion de estado de lectura

--- src/components/quote/quotes-list.jsx
+++ src/components/quote/quotes-list.jsx
@@ -3,6 +3,8 @@ import Modal from '@/components/modal/Modal'
 import axios from 'axios'
 import ModalAlert from "../modal/ModalAlert";
 import ModalDetails from "../modal/ModalDetails"
+import { Checkbox } from "flowbite-react";
+import { Input } from "postcss";
 
 
 
@@ -15,6 +17,32 @@ const ProductList = ({ quotes }) => {
   const [phoneQuote, setPhoneQuote] = useState('')
   const [messageQuote, setMessageQuote] = useState('')
   const [imgQuote, setImgQuote] = useState('')
+  const [estadoLecturaQuote, setEstadoLecturaQuote] = useState('')
+
+  //contenedor de quotes
+  const[quotesobj,setQuotesobj]=useState(quotes);
+  
+  //control checkbox
+  const [isChecked,setIsChecked] = useState(false);
+  
+  //Contenedores de para quote modificado
+  const [idDocumentEDIT, setIdDocumentEDIT] = useState('')
+
+const bodyObj = {
+  collection: "quotes",
+  id: idDocumentEDIT,
+document: {
+  firstName: firstNameQuote,
+  lastName: lastNameQuote,
+  message: messageQuote,
+  phone: phoneQuote,
+  email: emailQuote,
+  img: imgQuote,
+  leido: Boolean(true)
+}
+}
+
+
 
   //Contreol de  modales
   const [isOpenModalSeeDetails, setIsOpenModalSeeDetails] = useState(false)
@@ -34,34 +62,52 @@ const ProductList = ({ quotes }) => {
   headers.append("Access-Control-Allow-Credentials", "true");
   headers.append("GET", "POST", "OPTIONS");
 
+const getQuotes = async () => {
+  await fetch("http://localhost:5000/service/getDoc", {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      setFirstNameQuote(data.firstName)
+      setLastNameQuote(data.lastName)
+      setPhoneQuote(data.phone)
+      setEmailQuote(data.email)
+      setMessageQuote(data.message)
+      setImgQuote(data.img)
+      setLoadingData(!loadingData)
+      setIdDocument('')
+      setEstadoLecturaQuote(data.leido)
+    })
+    .catch((error) => {
+    });
+
+}
+
+const updateQuotes = async () => {
+
+
+  const body ={
+    collection: "quotes",
+
+  };
+  const res = await axios.post('http://localhost:5000/service/getCollection', body)
+  setQuotesobj(res.data);
+}
+
 
   useEffect(() => {
     if (idDocument !== "") {
-
-      fetch("http://localhost:5000/service/getDoc", {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify(body),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setFirstNameQuote(data.firstName)
-          setLastNameQuote(data.lastName)
-          setPhoneQuote(data.phone)
-          setEmailQuote(data.email)
-          setMessageQuote(data.message)
-          setImgQuote(data.img)
-          setLoadingData(!loadingData)
-          setIdDocument('')
-        })
-        .catch((error) => {
-        });
+      getQuotes();
+     
     }
-  }, [isOpenModalSeeDetails]);
+  }, [isOpenModalSeeDetails],[quotesobj]);
 
 
   const handleSeeDetails = (id) => {
     setIdDocument(id)
+    setIdDocumentEDIT(id)
 
   }
   const handleModalSeeDetails = () => {
@@ -75,10 +121,45 @@ const ProductList = ({ quotes }) => {
 
   }
 
+  //toshi
+  const handleCotizacionEstado = async (e) =>{
+    e.preventDefault();
+const res = await axios.put('http://localhost:5000/service/update',bodyObj)
+updateQuotes();
+handleCerrar();
+
+  }
+
+  const handleOnChange = () =>{
+    setIsChecked(!isChecked);
+
+    if(isChecked===false)
+    {
+      filtroEstado()
+      const quotesobjfiltrado = quotesobj.filter(quote => quote.leido===false);
+      setQuotesobj(quotesobjfiltrado);
+    }
+    else
+    {
+    updateQuotes();
+    }
+  }
+  
+
+  const filtroEstado = () => {
+
+console.log("Filttrando")
+
+  }
+
   return (
     <>
-      <div className="mt-10">
+      <div className="mt-7">
         <h1 className="text-2xl font-semibold mx-10 mb-10 text-black">Lista de Cotizaciones</h1>
+        <div className="flex justify-center">
+        <h3 className="text-base font-semibold mx-10 mb-10 text-black">Ver solo las pendientes</h3>
+          <input type="checkbox" checked={isChecked} onChange={handleOnChange}/>
+          </div>
         <div className="flex justify-center">
           <table className="table-auto mt-5">
             <thead className="text-white bg-primary">
@@ -93,10 +174,10 @@ const ProductList = ({ quotes }) => {
               </tr>
             </thead>
             <tbody className="white  text-black">
-              {quotes.map((quote, index) => (
+              {quotesobj.map((quote, index) => (
                 <tr key={quote.id}>
                   <td className="border border-black px-2 text-center">{index + 1}</td>
-                  <td className="border border-black px-3 w-30 text-center">{quote.firstName} </td>
+                  <td className="border border-black px-3 w-30 text-center">{(quote.leido==true)?"leido":"sin leer"} </td>
                   <td className="border border-black px-5 w-30 text-center overflow-hidden">{quote.lastName} </td>
                   <td className="border border-black px-3 w-20 text-center">{quote.phone}</td>
                   <td className="border border-black px-5 text-center">{quote.email}</td>
@@ -142,7 +223,7 @@ const ProductList = ({ quotes }) => {
                 <img src={imgQuote} alt="" width="200" height="200"/>
               </div>
               <div className="space-x-2 mt-4">
-                      <button  className=" text-white bg-primary px-2 py-1 rounded hover:bg-blue-600">Marcar como leído</button>
+                      <button onClick={handleCotizacionEstado} className=" text-white bg-primary px-2 py-1 rounded hover:bg-blue-600">Marcar como leído</button>
                       <button  className="bg-secondary text-white px-2 py-1 rounded hover:bg-red-600">Cancelar</button>
                     </div>
             </div>
