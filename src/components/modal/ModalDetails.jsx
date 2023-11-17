831f9ffd5757fb819630c50fc0be36d1d4e8df3a
Emmanuel Giron <emmus.giron@gmail.com>
2023-11-16 21:00:54 -0600

    Alertas ordenes y productos y cambios en el diseño en modal cotizaciones y ordenes

--- src/components/modal/ModalDetails.jsx
+++ src/components/modal/ModalDetails.jsx
@@ -5,9 +5,11 @@ const Modal = ({ handleModal, Title = "", children }) => {
     return (
         <article className={`modal fixed z-50 top-0 left-0 w-full min-h-screen bg-black bg-opacity-50 flex justify-center items-center ${handleModal && "flex"}`}>
             <div className="bg-white shadow-md relative p-4 overflow-y-auto rounded-lg mt-4 mb-4 w-full max-w-2xl min-h-150 max-h-[600px]">
-                <button className="modal-close absolute top-4 right-4 p-5 text-black" onClick={handleModal}>X</button>
+                <button className="modal-close absolute top-4 right-4 px-2 border border-black rounded-lg text-white font-bold bg-red-600" onClick={handleModal}>
+                    <p className='pb-1 ml-1 mr-1'>x</p>
+                </button>
                 <div className="flex justify-center">
-                    <h1 className="text-black p-5 font-bold uppercase">{Title}</h1>
+                    <h1 className="text-primary p-5 font-bold uppercase underline">{Title}</h1>
                 </div>
                 <div className="flex items-start h-full">
                     {children}
